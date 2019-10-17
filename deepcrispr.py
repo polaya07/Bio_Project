7ebf6a0232ab6296ac8df1e095b2e24b1ec7f714
Gao, Shang <gaos@ornl.gov>
2019-10-17 18:17:57 -0400

    full pipeline now functional

--- deepcrispr.py
+++ deepcrispr.py
@@ -1,3 +1,6 @@
+#import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="3"
+
 import tensorflow as tf
 import numpy as np
 import sys
@@ -13,45 +16,36 @@ class deepcrispr(object):
         #inputs
         self.inputs = tf.placeholder(tf.float32,shape=[None,23,4])
         self.training = tf.placeholder(tf.bool)
+        self.dropout = tf.placeholder(tf.float32)
     
         #encoder layers
         conv1 = tf.layers.conv1d(self.inputs,32,3,1,'same',
                 activation=tf.nn.relu,kernel_initializer=self.initializer)
         bn_fw1 = tf.layers.batch_normalization(conv1,training=self.training)
-        noise1 = tf.cond(self.training,
-                 lambda:tf.add(bn_fw1,tf.random.normal(tf.shape(bn_fw1),0,0.1)),
-                 lambda:bn_fw1)
+        drop1 = tf.nn.dropout(bn_fw1,self.dropout)
         
-        conv2 = tf.layers.conv1d(noise1,64,3,2,'same',
+        conv2 = tf.layers.conv1d(drop1,64,3,2,'same',
                 activation=tf.nn.relu,kernel_initializer=self.initializer)    
-        bn_fw2 = tf.layers.batch_normalization(conv2,training=self.training)      
-        noise2 = tf.cond(self.training,
-                 lambda:tf.add(bn_fw2,tf.random.normal(tf.shape(bn_fw2),0,0.1)),
-                 lambda:bn_fw2)
+        bn_fw2 = tf.layers.batch_normalization(conv2,training=self.training)
+        drop2 = tf.nn.dropout(bn_fw2,self.dropout)    
         
-        conv3 = tf.layers.conv1d(noise2,64,3,1,'same',
+        conv3 = tf.layers.conv1d(drop2,64,3,1,'same',
                 activation=tf.nn.relu,kernel_initializer=self.initializer)    
-        bn_fw3 = tf.layers.batch_normalization(conv3,training=self.training)      
-        noise3 = tf.cond(self.training,
-                 lambda:tf.add(bn_fw3,tf.random.normal(tf.shape(bn_fw3),0,0.1)),
-                 lambda:bn_fw3)
+        bn_fw3 = tf.layers.batch_normalization(conv3,training=self.training)
+        drop3 = tf.nn.dropout(bn_fw3,self.dropout)  
         
-        conv4 = tf.layers.conv1d(noise3,128,3,2,'same',
+        conv4 = tf.layers.conv1d(drop3,128,3,2,'same',
                 activation=tf.nn.relu,kernel_initializer=self.initializer)    
-        bn_fw4 = tf.layers.batch_normalization(conv4,training=self.training)      
-        noise4 = tf.cond(self.training,
-                 lambda:tf.add(bn_fw4,tf.random.normal(tf.shape(bn_fw4),0,0.1)),
-                 lambda:bn_fw4)
+        bn_fw4 = tf.layers.batch_normalization(conv4,training=self.training)
+        drop4 = tf.nn.dropout(bn_fw4,self.dropout)
 
-        conv5 = tf.layers.conv1d(noise4,128,3,1,'same',
+        conv5 = tf.layers.conv1d(drop4,128,3,1,'same',
                 activation=tf.nn.relu,kernel_initializer=self.initializer)    
-        bn_fw5 = tf.layers.batch_normalization(conv5,training=self.training)      
-        noise5 = tf.cond(self.training,
-                 lambda:tf.add(bn_fw5,tf.random.normal(tf.shape(bn_fw5),0,0.1)),
-                 lambda:bn_fw5)
+        bn_fw5 = tf.layers.batch_normalization(conv5,training=self.training)
+        drop5 = tf.nn.dropout(bn_fw5,self.dropout)
         
         #latent embedding
-        self.embed = tf.contrib.layers.flatten(noise5)
+        self.embed = tf.contrib.layers.flatten(drop5)
         reshape = tf.reshape(self.embed,(-1,6,1,128))
         
         #deconvolution layers
@@ -90,6 +84,8 @@ class deepcrispr(object):
         array = np.zeros((batch_size,23,4))
         for i,seq in enumerate(batch):
             for j,bp in enumerate(seq):
+                if bp not in self.bp2idx:
+                    continue
                 p = self.bp2idx[bp]
                 array[i,j,p] = 1
                 
@@ -125,7 +121,7 @@ class deepcrispr(object):
                 batch = shuffled[start:stop]
                 batch = self._str_to_numpy(batch)
 
-                feed_dict = {self.inputs:batch,self.training:True}
+                feed_dict = {self.inputs:batch,self.training:True,self.dropout:0.9}
                 loss,_ = self.sess.run([self.loss,self.optimizer],feed_dict=feed_dict)
                 train_loss.append(loss)
                 
@@ -165,7 +161,7 @@ class deepcrispr(object):
     
             batch = X[start:stop]
             batch = self._str_to_numpy(batch)
-            feed_dict = {self.inputs:batch,self.training:False}
+            feed_dict = {self.inputs:batch,self.training:False,self.dropout:1.0}
             loss = self.sess.run(self.loss,feed_dict=feed_dict)
             total_loss.append(loss)
             
@@ -188,7 +184,7 @@ class deepcrispr(object):
     
             batch = X[start:stop]
             batch = self._str_to_numpy(batch)
-            feed_dict = {self.inputs:batch,self.training:False}
+            feed_dict = {self.inputs:batch,self.training:False,self.dropout:1.0}
             embeds = self.sess.run(self.embed,feed_dict=feed_dict)
             all_embeds.append(embeds)
             
