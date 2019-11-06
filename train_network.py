bcff765baedb9856e574da4cabaf0d163f9035dd
Gao, Shang <gaos@ornl.gov>
2019-11-06 15:47:50 -0500

    adding pretraining loop to train_network

--- train_network.py
+++ train_network.py
@@ -6,6 +6,48 @@ import sys
 import random
 from deepcrispr import deepcrispr
 
+'''
+pretraining
+'''
+
+'''
+#data directory
+data_dir = '../PAM_Sites'
+
+#PAM strings
+data = []
+
+#load files
+for i,genome in enumerate(glob.glob(os.path.join(data_dir,'*.txt'))):                                       
+    with open(genome) as f:
+        text = f.readlines()
+    text = [l[:20]+l[21:24] for l in text[1:]]
+    data.extend(text)
+    
+    sys.stdout.write("processing file %i       \r" % (i+1))
+    sys.stdout.flush()
+                
+print('\nsaved %i pam sites' % len(data))
+    
+#train val split
+print('shuffling data into train/val splits')
+num_samples = len(data)
+val_size = int(0.2 * num_samples)
+#random.shuffle(data)
+seqs = data[:-val_size]
+val_seqs = data[-val_size:]
+'''
+#train model
+if not os.path.exists('savedmodels'):
+    os.makedirs('savedmodels')
+
+model = deepcrispr()
+#model.pretrain(seqs,X_val=val_seqs,savepath='savedmodels/deepcrispr.ckpt')
+
+'''
+training
+'''
+
 #data directory
 data_dir = '../Ecoli_Training_dataset'
 
@@ -47,9 +89,7 @@ train_labels = train_labels[:-val_size]
 val_labels = train_labels[-val_size:]
 
 #train model
-if not os.path.exists('savedmodels'):
-    os.makedirs('savedmodels')
-
-model = deepcrispr()
-#model.pretrain(train_data,X_val=val_data,savepath='savedmodels/deepcrispr.ckpt')
+#model.load('savedmodels/deepcrispr.ckpt')
 model.train(train_data,train_labels,val_data,val_labels,savepath='savedmodels/deepcrispr.ckpt')
+fscore = model.fscore(test_data,test_labels)
+print('test set fscore: %.6f' % fscore)
